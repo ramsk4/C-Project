bdaa00634d3ccf6ae3daeebc9ec37acd331d8962
Shriram Kulkarni <41006662+ramsk4@users.noreply.github.com>
2026-01-28 19:09:26 +0530

    Add files via upload

---
+++ FizzBuzzProgram.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class FizzBuzzProgram
+    {
+        public static void FizzBuzz()
+        {
+
+            for (int i = 1; i < 100; i++)
+            {
+
+                if ( i % 3 == 0)
+                {
+                    Console.WriteLine("----\nFizz");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("----\nBuzz");
+                }
+
+                else 
+                {
+                    Console.WriteLine("----\n"+" "+i);
+                }
+            }
+
+               
+        }
+
+        public static void GuessTheNumber()
+        {
+            Random random = new Random();
+            int on = random.Next(1, 101);
+
+            int un = 0;
+
+            while(true)
+            {
+                Console.WriteLine("Gusess the number");
+                un= Convert.ToInt32(Console.ReadLine());    
+
+                if(un==on)
+                {
+                    Console.WriteLine("You Got the Wright Number");
+                    break;
+                }
+
+                if (un < on)
+                {
+                    Console.WriteLine("You got lower");
+                }
+
+                if (un > on)
+                {
+                    Console.WriteLine("You got Higher");
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp3;
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Fizz Buzz 
+            // FizzBuzzProgram.FizzBuzz();
+            FizzBuzzProgram.GuessTheNumber();   
+
+    
+
+
+            //int sum = MathFunctions.add(10, 20);
+            //Console.WriteLine("Sum is :" + sum);
+
+
+        }
+    }
+}
+
+
+
+
